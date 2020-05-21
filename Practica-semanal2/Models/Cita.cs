7edfd9be1c850a72702610501448649069938a99
Yonathan-Roblero-Soto <yonathan.roblero17@tectijuana.edu.mx>
2020-05-20 21:46:08 -0700

    Initial commit

---
+++ Practica-semanal2/Models/Cita.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Practica_semanal2.Models
+{
+    public class Cita
+    {
+        public int id { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public int Cantidad { get; set; }
+        public string Status { get; set; }
+    }
+}
